fca973fd3751991cca4cac1d97b6af7b8618b575
TarikBeg <44277921+TarikBeg@users.noreply.github.com>
2020-06-30 08:17:57 +0200

    uno adding about page

---
+++ TestAppUno/TestAppUno.Shared/Content/AboutPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using JoesBurgerStore.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TestAppUno.Shared.Content
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AboutPage : Page
+    {
+        public AboutPage()
+        {
+            this.InitializeComponent();
+
+            this.DataContext = new AboutViewModel();
+        }
+    }
+}
---
+++ TestAppUno/TestAppUno.Shared/ViewModel/AboutViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using JoesBurgerStore.Contracts;
+using JoesBurgerStore.Contracts.ViewModels;
+using TestAppUno.ViewModels.Base;
+using Xamarin.Forms;
+
+namespace JoesBurgerStore.ViewModels
+{
+    public class AboutViewModel : ViewModelBase
+    {
+        private string _general;
+        private string _copyright;
+        private string _moreInfo;
+        //private string _link;
+
+        public string General
+        {
+            get { return _general; }
+            set { _general = value; OnPropertyChanged("General"); }
+        }
+
+        public string Copyright
+        {
+            get { return _copyright; }
+            set { _copyright = value; OnPropertyChanged("Copyright"); }
+        }
+
+        public string MoreInfo
+        {
+            get { return _moreInfo; }
+            set { _moreInfo = value; OnPropertyChanged("MoreInfo"); }
+        }
+
+        public ICommand MoreInfoCommand { get; set; }
+
+        public AboutViewModel()
+        {
+            InitializeData();
+            InitializeCommands();
+        }
+
+        private void InitializeCommands()
+        {
+            MoreInfoCommand = new Command(() =>
+            {
+                Device.OpenUri(new Uri("http://www.google.be"));
+            });
+        }
+
+        private void InitializeData()
+        {
+            //TODO: get from resx
+            General = "App from Joe's Hamburgers";
+            Copyright = "Copyright 2016 Joe - All rights reserved";
+            MoreInfo = "Visit us on our website";
+            
+        }
+    }
+}
